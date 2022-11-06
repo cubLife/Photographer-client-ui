a9cd24467a115d5d05cff6183704e92cf694802e
Serhii <sergick6690@gmail.com>
2022-11-06 19:49:17 +0100

    Changed  font-size of .title

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,10 +4,7 @@ export default class Contact extends Component {
   render() {
     return (
       <div>
-        <h1
-          className="text-center text-uppercase mt-5"
-          style={{ color: "#3f4b5b" }}
-        >
+        <h1 className="text-center  mt-5" style={{ color: "#3f4b5b" }}>
           Skontaktuj się ze mną
         </h1>
         <ContactLayout />
