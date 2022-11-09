e6e55e42468c2abdcf8db93a318174a918382f0e
Serhii <sergick6690@gmail.com>
2022-11-09 20:38:59 +0100

    deleted unnecessary imports

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import axios from "axios";
-import CardGrid from "../components/Layout/PhotoAlbumLayout";
-import PhotoAlbumLayout from "../components/Layout/PhotoAlbumLayout";
 import PhotoSessionsLayout from "../components/Layout/PhotoSessionsLayout";
 
 export default class Portfolio extends Component {
